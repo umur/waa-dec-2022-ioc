721e26d6b533617d7cb4b33f5a611c34e28ea55e
Prabesh KC <kcprabase@gmail.com>
2022-11-29 00:38:12 -0600

    Added files for project
    Added files for project

---
+++ src/main/java/com/w1d1/ioc/IocApplication.java
@@ -0,0 +1,20 @@
+package com.w1d1.ioc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IocApplication {
+    public static void main(String[] args) {
+        var injector = new MyInjector();
+        try {
+//            injector.getBean(ClassA.class).toString();
+            injector.getBean(ClassB.class).toString();
+            injector.getBean(ClassC.class).toString();
+        } catch (BeanNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        SpringApplication.run(IocApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/w1d1/ioc/IocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.w1d1.ioc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
