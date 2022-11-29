2fe7184dc8725d624a86f3aab271db80bac2b43e
caranstha <caranofficial@gmail.com>
2022-11-29 14:27:16 -0600

    injector added

--- src/main/java/com/waalab1/ioc/IocApplication.java
+++ src/main/java/com/waalab1/ioc/IocApplication.java
@@ -8,6 +8,17 @@ public class IocApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(IocApplication.class, args);
+		MyInjector injector = new MyInjector();
+		try {
+			var instanceOfAClass = injector.getBean(AClass.class);
+//			var instanceOfBClass = injector.getBean(BClass.class);
+//			var instanceOfCClass = injector.getBean(CClass.class);
+			System.out.println(instanceOfAClass.toString());
+//			System.out.println(instanceOfBClass.toString());
+//			System.out.println(instanceOfCClass.toString());
+		} catch (BeanNotFoundException e) {
+			throw new RuntimeException(e);
+		}
 	}
 
 }
--- src/main/java/com/waalab1/ioc/MyInjector.java
+++ src/main/java/com/waalab1/ioc/MyInjector.java
@@ -1,4 +1,49 @@
 package com.waalab1.ioc;
+import org.reflections.Reflections;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MyInjector {
+    Map<Class<?>, Object> instances = new HashMap<>();
+    public MyInjector() {
+        Reflections reflections = new Reflections("com.waalab1.ioc");
+        reflections.getTypesAnnotatedWith(MyBean.class).forEach(cls -> {
+            try {
+                Object t = cls.getDeclaredConstructor().newInstance();
+                Arrays.stream(cls.getFields()).filter(c -> c.isAnnotationPresent(MyAutowired.class))
+                        .toList()
+                        .forEach(field -> {
+                            Class<?> innerClass = field.getType();
+                            try {
+                                Object innerInstance = innerClass.getDeclaredConstructor().newInstance();
+                                instances.put(innerClass, innerInstance);
+                                field.set(t, innerInstance);
+                            } catch (InstantiationException | IllegalAccessException e) {
+                                throw new RuntimeException(e);
+                            } catch (InvocationTargetException e) {
+                                throw new RuntimeException(e);
+                            } catch (NoSuchMethodException e) {
+                                throw new RuntimeException(e);
+                            }
+                        });
+                instances.put(cls, t);
+            } catch (InstantiationException | IllegalAccessException e) {
+                throw new RuntimeException(e);
+            } catch (InvocationTargetException e) {
+                throw new RuntimeException(e);
+            } catch (NoSuchMethodException e) {
+                throw new RuntimeException(e);
+            }
+        });
+    }
+    public Object getBean(Class<?> cls) throws BeanNotFoundException {
+        try {
+            return instances.get(cls);
+        } catch (Exception e) {
+            throw new BeanNotFoundException("Error");
+        }
+    }
 }
