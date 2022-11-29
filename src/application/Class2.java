5f5c3bc93fe2cd9251b478156e883a7296198572
gurung <binodgurung@gurungs-MacBook-Air.local>
2022-11-29 13:11:11 -0600

    W1D1 assignment pushed

---
+++ src/application/Class2.java
@@ -0,0 +1,18 @@
+package application;
+
+import framework.MyAutoWired;
+import framework.MyBean;
+
+@MyBean
+public class Class2 {
+    @MyAutoWired
+    public Class1 c1;
+
+    @MyAutoWired
+    public Class3 c3;
+    public void print(){
+        c1.print();
+        System.out.println("Hello from Class2");
+        c3.print();
+    }
+}
