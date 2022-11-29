e6e62946532943df55b2b0f62efe7cd10ec3c070
Nirajan Karki <nirajanchelsea@gmail.com>
2022-11-29 09:31:16 -0600

    'waa lab1 '

---
+++ src/main/java/com/example/demo/BeanNotFoundException.java
@@ -0,0 +1,10 @@
+package com.example.demo;
+
+public class BeanNotFoundException extends Exception {
+    public BeanNotFoundException() {
+    }
+
+    public BeanNotFoundException(String message) {
+        super(message);
+    }
+}
