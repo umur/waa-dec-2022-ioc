5c7c30ff5889dab1a962a8a8ee9f67759b10f138
eudism <115727438+eudism@users.noreply.github.com>
2022-11-30 07:16:00 -0600

    Lab1

---
+++ Lab1/src/application/ClassA.java
@@ -0,0 +1,11 @@
+package application;
+
+import framework.MyBean;
+
+@MyBean
+public class ClassA {
+    public void message(){
+
+        System.out.println("Class A message");
+    }
+}
---
+++ Lab1/src/application/ClassC.java
@@ -0,0 +1,18 @@
+package application;
+
+import framework.MyAutoWired;
+import framework.MyBean;
+
+@MyBean
+public class ClassC {
+    @MyAutoWired
+    private ClassA a;
+
+    @MyAutoWired
+    private ClassB b;
+    public void message(){
+        a.message();
+        System.out.println("Hello from Class2");
+        b.message();
+    }
+}
